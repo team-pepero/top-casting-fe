e710cff52a3c318c901973aa5185fb1c374d30e9
B-singularity <skil1489@naver.com>
2024-03-18 15:31:19 +0900

    feat:아이템 리스트 페이지 사진 크기 조정

--- src/component/ItemList.js
+++ src/component/ItemList.js
@@ -1,37 +1,39 @@
 import React from 'react';
-import { Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
+import { Grid, Card, CardMedia, CardContent, Typography, CardActions, Button } from '@mui/material';
 
 const ItemList = ({ items }) => {
   return (
     <div>
-  <Typography variant="h4" gutterBottom>
-    전체 아이템
-  </Typography>
-  <Grid container spacing={2}>
-    {items.map((item, index) => (
-      <Grid item xs={12} sm={6} md={4} key={item.id}>
-        <Card>
-          <CardMedia
-            component="img"
-            height="140"
-            image={item.imageUrl}
-            alt={`Item ${index}`}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {item.itemName}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {item.itemPrice}원
-            </Typography>
-          </CardContent>
-        </Card>
+      <Typography variant="h4" gutterBottom>
+        전체 아이템
+      </Typography>
+      <Grid container spacing={3}>
+        {items.map((item, index) => (
+          <Grid item xs={12} sm={6} md={4} key={item.id}>
+            <Card style={{ width: '100%' }}>
+              <CardMedia
+                component="div"
+                style={{ height: 0, paddingTop: '100%', backgroundSize: 'cover', backgroundImage: `url(${item.imageUrl})` }}
+                alt={`Item ${index}`}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {item.itemName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {item.itemPrice}원
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">더보기</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
       </Grid>
-    ))}
-  </Grid>
-</div>
-
+    </div>
   );
 };
 
 export default ItemList;
+
--- src/page/ItemListPage.js
+++ src/page/ItemListPage.js
@@ -2,25 +2,18 @@ import React from 'react';
 import { Grid, Card, CardMedia, CardContent, Typography, CardActions, Button } from '@mui/material';
 import { useLocation } from 'react-router-dom';
 
-
 const ItemListPage = () => {
     const location = useLocation();
-
     const { items } = location.state || { items: { content: [] } }; // 기본값 설정
 
-    console.log(`ItemListPage items.content: `, JSON.stringify(items.content));
-
-    
-
     return (
-        <Grid container spacing={4}>
+        <Grid container spacing={3}>
             {items.content.map((item) => (
                 <Grid item xs={12} sm={6} md={4} key={item.itemId}>
-                    <Card>
+                    <Card style={{ width: '100%' }}>
                         <CardMedia
-                            component="img"
-                            height="140"
-                            image={item.imageUrl}
+                            component="div"
+                            style={{ height: 0, paddingTop: '100%', backgroundSize: 'cover', backgroundImage: `url(${item.imageUrl})` }}
                             alt={item.itemName}
                         />
                         <CardContent>
@@ -30,17 +23,15 @@ const ItemListPage = () => {
                             <Typography variant="body2" color="text.secondary">
                                 가격: {item.itemPrice}원
                             </Typography>
-                            {/* 추가 정보 표시 가능 */}
                         </CardContent>
                         <CardActions>
                             <Button size="small">더보기</Button>
-                            {/* 필요한 액션 추가 */}
                         </CardActions>
                     </Card>
                 </Grid>
             ))}
         </Grid>
-      );
+    );
 };
 
 export default ItemListPage;
