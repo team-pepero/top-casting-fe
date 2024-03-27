023cf26bab3a3965bd26f242160c057f48b00ae3
nnWon <nwwn4627@gmail.com>
2024-03-27 16:18:05 +0900

    design: 장바구니로 이동 모달이 화면 중앙에 위치하도록 수정

--- src/page/ItemDetailPage.js
+++ src/page/ItemDetailPage.js
@@ -115,31 +115,6 @@ const ItemDetailPage = () => {
 
                             <div class="flex items-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8 mt-8 md:mt-12">
                                 <button onClick={handleAddToCart} class="w-full md:w-3/5 border border-gray-800 text-base font-medium leading-none text-white uppercase py-6 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-gray-800 text-white dark:bg-white dark:text-gray-900 dark:hover:bg-gray-200">Add to Cart</button>
-                                {showModal && (
-                                    <div class="relative flex justify-center items-center">
-                                        <div id="menu" class="w-full h-full bg-gray-900 bg-opacity-80 top-0 fixed sticky-0">
-                                            <div class="2xl:container  2xl:mx-auto py-48 px-4 md:px-28 flex justify-center items-center">
-                                                <div class="w-96 md:w-auto dark:bg-gray-800 relative flex flex-col justify-center items-center bg-white py-16 px-4 md:px-24 xl:py-24 xl:px-36">
-                                                    <div role="banner"></div>
-                                                    <div class="mt-12">
-                                                        <h1 role="main" class="text-3xl dark:text-white lg:text-4xl font-semibold leading-7 lg:leading-9 text-center text-gray-800">장바구니로 이동하시겠습니까?</h1>
-                                                    </div>
-                                                    <div class="mt">
-                                                        <p class="mt-6 sm:w-80 text-base dark:text-white leading-7 text-center text-gray-800">장바구니에 상품이 담겼습니다.</p>
-                                                    </div>
-                                                    <button onClick={handleSweetClick} class="w-full dark:text-gray-800 dark:hover:bg-gray-100 dark:bg-white sm:w-auto mt-14 text-base leading-4 text-center text-white py-6 px-16 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-gray-800 hover:bg-black">장바구니로 이동</button>
-                                                    <a href="javascript:void(0)" onClick={handleCloseModal} class="mt-6 dark:text-white dark:hover:border-white text-base leading-none focus:outline-none hover:border-gray-800 focus:border-gray-800 border-b border-transparent text-center text-gray-800">계속 쇼핑하기</a>
-                                                    <button onClick={handleCloseModal} class="text-gray-800 dark:text-gray-400 absolute top-8 right-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800" aria-label="close">
-                                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M18 6L6 18" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
-                                                            <path d="M6 6L18 18" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
-                                                        </svg>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )}
                             </div>
                         </div>
                     </div >
@@ -148,6 +123,33 @@ const ItemDetailPage = () => {
                     <img src={itemDetail.itemDetailedImageUrl} alt="Detailed view" class="mx-auto w-full h-full" />
                 </div>
             </div >
+            {showModal && (
+                <div class="flex justify-center items-center h-screen">
+                    <div id="menu" class="w-full h-full bg-gray-900 bg-opacity-80 top-0 fixed sticky-0">
+                        <div class="2xl:container 2xl:mx-auto flex justify-center items-center h-screen">
+                            <div class="w-96 md:w-auto dark:bg-gray-800 relative flex flex-col justify-center items-center bg-white py-16 px-4 md:px-24 xl:py-24 xl:px-36">
+                                <div role="banner"></div>
+                                <div class="mt-12">
+                                    <h1 role="main" class="text-3xl dark:text-white lg:text-4xl font-semibold leading-7 lg:leading-9 text-center text-gray-800">장바구니로 이동하시겠습니까?</h1>
+                                </div>
+                                <div class="mt">
+                                    <p class="mt-6 sm:w-80 text-base dark:text-white leading-7 text-center text-gray-800">장바구니에 상품이 담겼습니다.</p>
+                                </div>
+                                <button onClick={handleSweetClick} class="w-full dark:text-gray-800 dark:hover:bg-gray-100 dark:bg-white sm:w-auto mt-14 text-base leading-4 text-center text-white py-6 px-16 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-gray-800 hover:bg-black">장바구니로 이동</button>
+                                <a href="javascript:void(0)" onClick={handleCloseModal} class="mt-6 dark:text-white dark:hover:border-white text-base leading-none focus:outline-none hover:border-gray-800 focus:border-gray-800 border-b border-transparent text-center text-gray-800">계속 쇼핑하기</a>
+                                <button onClick={handleCloseModal} class="text-gray-800 dark:text-gray-400 absolute top-8 right-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800" aria-label="close">
+                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M18 6L6 18" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M6 6L18 18" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+
 
         </>
 
