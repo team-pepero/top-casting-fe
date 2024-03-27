6fdade7d0a99fa7df380afac07e9fa5d5355d294
nnWon <nwwn4627@gmail.com>
2024-03-27 11:41:01 +0900

    design: 아이템 디테일 페이지 디자인 추가

--- src/page/ItemDetailPage.js
+++ src/page/ItemDetailPage.js
@@ -1,17 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import {
-    Grid,
-    Card,
-    CardMedia,
-    Typography,
-    Box,
-    Stack,
-    Select,
-    MenuItem,
-    TextField,
-    Button,
-} from "@mui/material";
 import { HttpGet } from "../service/HttpService";
 import { HttpPost } from "../service/HttpService";
 
@@ -64,83 +52,63 @@ const ItemDetailPage = () => {
         setQuantity(Math.max(1, event.target.value)); // 수량은 최소 1 이상
     };
 
-    if (!itemDetail) return <Typography>Loading...</Typography>;
+    if (!itemDetail) return <div>Loading...</div>;
 
     return (
-        <Box sx={{ flexGrow: 1, p: 4 }}>
-            <Grid container spacing={4}>
-                <Grid item xs={12} md={6}>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image={itemDetail.itemImageUrl}
-                            alt={itemDetail.itemName}
-                            sx={{ height: "100%", width: "100%", objectFit: "cover" }}
-                        />
-                        {/* 상세 이미지 추가 */}
-                        <CardMedia
-                            component="img"
-                            image={itemDetail.itemDetailedImageUrl}
-                            alt="Detailed view"
-                            sx={{ height: "100%", width: "100%", objectFit: "cover", mt: 2 }}
-                        />
-                    </Card>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <Typography gutterBottom variant="h4" component="div">
-                        {itemDetail.itemName}
-                    </Typography>
-                    <Typography gutterBottom variant="h5" color="text.secondary">
-                        {itemDetail.itemPrice}
-                    </Typography>
-                    <Typography variant="body2">
-                        Main Category ID: {itemDetail.mainCategoryId}
-                    </Typography>
-                    <Typography variant="body2">
-                        Sub Category ID: {itemDetail.subCategoryId}
-                    </Typography>
-                    <Box>
-                        <Typography variant="body1" gutterBottom>
-                            Colors Available:
-                        </Typography>
-                    </Box>
-                    <Stack direction="row" spacing={2} alignItems="center">
-                        <Select
-                            labelId="color-select-label"
-                            id="color-select"
-                            value={selectedColor}
-                            label="Color"
-                            onChange={handleColorChange}
-                        >
-                            {itemDetail.itemColors.map((color) => (
-                                <MenuItem key={color.optionId} value={color.optionId}>
-                                    {`${color.colorName} - Stock: ${color.stock}`}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                        <TextField
-                            label="Quantity"
-                            type="number"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            variant="outlined"
-                            value={quantity}
-                            onChange={handleQuantityChange}
-                            sx={{ mt: 2, width: "100px" }}
-                        />
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={handleAddToCart}
-                            sx={{ mt: 2 }}
-                        >
-                            Add to Cart
-                        </Button>
-                    </Stack>
-                </Grid>
-            </Grid>
-        </Box>
+
+        <>
+            <div class="2xl:container 2xl:mx-auto md:py-12 lg:px-20 md:px-6 py-9 px-4">
+                <div id="viewerButton" class="hidden w-full flex justify-center">
+                    <button onclick="openView()" class="bg-white text-indigo-600 shadow-md rounded focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 py-5 px-10 font-semibold">Open Quick View</button>
+                </div>
+                <div id="viewerBox" class="lg:p-10 md:p-6 p-4 bg-white dark:bg-gray-900">
+                    <div class="mt-3 md:mt-4 lg:mt-0 flex flex-col lg:flex-row items-strech justify-center lg:space-x-8">
+                        <div class="lg:w-1/2 flex justify-between items-strech bg-white-50  px-2 py-20 md:py-6 md:px-6 lg:py-24">
+                            <div>
+                                <img src={itemDetail.itemImageUrl} alt={itemDetail.itemName} class="w-full h-full" />
+                            </div>
+                        </div>
+                        <div class="lg:w-1/2 flex flex-col justify-center mt-7 md:mt-8 lg:mt-0 pb-8 lg:pb-0">
+                            <h1 class="text-3xl lg:text-4xl font-semibold text-gray-800 dark:text-white">{itemDetail.itemName}</h1>
+                            <div class="flex flex-col justify-start items-start w-full space-y-4">
+                                <p class="text-xl md:text-2xl leading-normal text-gray-800 dark:text-gray-50"></p>
+                                <p class="text-l md:text-xl font-semibold leading-none text-gray-600 dark:text-white">₩ {itemDetail.itemPrice} </p>
+                            </div>
+                            <p class="text-base leading-normal text-gray-600 dark:text-white mt-2">You don't just want to be comfortable sitting in a bar stool—you want to be comfortable shimmying it up to the bar, closer to your lover, or back slightly to include a third person in the conversation.</p>
+
+                            <div class="mt-8 flex-col">
+                                <div>
+                                    <select id="color-select" value={selectedColor} onChange={handleColorChange} class="border rounded-tl rounded-tr border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600">
+                                        <option disabled selected>Color</option>
+                                        {itemDetail.itemColors.map((color) => (
+                                            <option key={color.optionId} value={color.optionId}>
+                                                {color.colorName}
+                                            </option>
+                                        ))}
+                                    </select>
+                                </div>
+                                <input
+                                    class="border rounded-bl rounded-br border-gray-300 p-4 w-full text-base leading-4 placeholder-gray-600 text-gray-600"
+                                    type="number"
+                                    value={quantity}
+                                    onChange={handleQuantityChange}
+                                />
+                            </div>
+
+                            <div class="flex items-center flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6 lg:space-x-8 mt-8 md:mt-12">
+                                <button onClick={handleAddToCart} class="w-full md:w-3/5 border border-gray-800 text-base font-medium leading-none text-white uppercase py-6 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800 bg-gray-800 text-white dark:bg-white dark:text-gray-900 dark:hover:bg-gray-200">Add to Cart</button>
+                            </div>
+                        </div>
+                    </div >
+                </div >
+                <div class="mx-auto p-16 bg-gray-100 dark:bg-gray-800 flex flex-col w-3/4">
+                    <img src={itemDetail.itemDetailedImageUrl} alt="Detailed view" class="mx-auto w-full h-full" />
+                </div>
+            </div >
+
+
+        </>
+
     );
 };
 
